7f540bf9aeeb1478abc4e3944dfa3bb1904cad48
fanfan <1078189582@qq.com>
2022-08-01 18:09:38 +0800

    5.搭建了服务模块server并且使用了mybatis-generator代码自动生成工具

--- system/src/main/java/com/course/system/config/SystemApplication.java
+++ system/src/main/java/com/course/system/config/SystemApplication.java
@@ -1,11 +1,13 @@
-package com.course.system;
+package com.course.system.config;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
-
+import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @EnableEurekaClient
+@ComponentScan("com.course")
+@MapperScan("com.course.server.mapper")
 public class SystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(SystemApplication.class, args);
--- system/src/main/java/com/course/system/controller/TestController.java
+++ system/src/main/java/com/course/system/controller/TestController.java
@@ -1,13 +1,23 @@
 package com.course.system.controller;
 
+import com.course.server.domain.Test;
+import com.course.server.service.TestService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import java.util.List;
+
 @RestController//用来返回json格式的数据。如果要返回一个页面，那就用@Controller注解
 public class TestController {
-    @RequestMapping("/test")
-    public String test(){
+    @Resource
+    private TestService testService;
+    @RequestMapping("/one")
+    public String est(){
         return "okk";
     }
-
+    @RequestMapping("/test")
+    public List<Test> test(){
+        return testService.list();
+    }
 }
