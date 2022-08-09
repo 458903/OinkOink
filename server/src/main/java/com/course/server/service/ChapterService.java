b9562adb746964c1cffc2763c90b553e114763fb
fanfan <1078189582@qq.com>
2022-08-09 23:54:40 +0800

    23.集成分页插件pagehelper,注意页码从1开始

--- server/src/main/java/com/course/server/service/ChapterService.java
+++ server/src/main/java/com/course/server/service/ChapterService.java
@@ -4,6 +4,7 @@ import com.course.server.domain.Chapter;
 import com.course.server.domain.ChapterExample;
 import com.course.server.dto.ChapterDto;
 import com.course.server.mapper.ChapterMapper;
+import com.github.pagehelper.PageHelper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +17,9 @@ import java.util.List;
 public class ChapterService {
     @Resource
     private ChapterMapper chapterMapper;
+
     public List<ChapterDto> list(){
+       PageHelper.startPage(2,5);
         ChapterExample chapterExample=new ChapterExample();
 
 
